443aff5890c8fe8abe9c3cc0bb81ad720adf3dab
unknown <anna@gmail.com>
2024-09-05 11:04:24 +0530

    new commit

---
+++ finaltest/src/main/java/practice/finaltest/App.java
@@ -0,0 +1,34 @@
+package practice.finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to spring boot greeting app";
+	}
+	@GetMapping("/greeting")
+	public String greeting() {
+		return "greetings from spring boot greeting app";
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello from spring boot greeting app";
+	}
+	@GetMapping("/hi")
+	public String hi() {
+		return "hi from spring boot greeting app";
+	}
+	
+
+}
